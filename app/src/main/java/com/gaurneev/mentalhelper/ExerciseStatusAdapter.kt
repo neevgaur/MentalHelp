79252db62e4e2ed6727eee8c2a15ff3095936843
Shivang_Yadav <shivangyadav469@gmail.com>
2023-09-19 06:48:15 +0530

    First commit

---
+++ app/src/main/java/com/gaurneev/mentalhelper/ExerciseStatusAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.a7minuteworkout
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.a7minuteworkout.databinding.IitemExerciseStatusBinding
+
+class ExerciseStatusAdapter(val items: ArrayList<ExerciseModel>): RecyclerView.Adapter<ExerciseStatusAdapter.ViewHolder>() {
+
+    class ViewHolder(binding: IitemExerciseStatusBinding): RecyclerView.ViewHolder(binding.root)
+    {
+        val tvItem= binding.tvItem
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(IitemExerciseStatusBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val model: ExerciseModel= items[position]
+        holder.tvItem.text= model.getId().toString()
+
+        when{
+            model.getIsSelected()->{
+                holder.tvItem.background= ContextCompat.getDrawable(holder.itemView.context, R.drawable.item_circular_thin_color_accent_background)
+                holder.tvItem.setTextColor(Color.parseColor("#06d6a0"))
+            }
+            model.getIsCompleted()->{
+                holder.tvItem.background= ContextCompat.getDrawable(holder.itemView.context, R.drawable.item_circular_color_accent_background)
+                holder.tvItem.setTextColor(Color.parseColor("#184e77"))
+            }
+                    else->{
+                        holder.tvItem.background= ContextCompat.getDrawable(holder.itemView.context, R.drawable.item_circular_color_gray_background)
+                        holder.tvItem.setTextColor(Color.parseColor("#023047"))
+                    }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
--- app/src/main/java/com/gaurneev/mentalhelper/MainActivity.kt
+++ app/src/main/java/com/gaurneev/mentalhelper/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.gaurneev.mentalhelper
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.gaurneev.mentalhelper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding= null
     }
 }
